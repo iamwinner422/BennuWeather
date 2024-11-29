15de31766f8c97d43bdf060ad40a6bf1c51038e8
iamwinner422 <iamwinner422@gmail.com>
2024-11-29 01:09:52 +0000

    cutomizing ButtonSection

--- src/components/ButtonSection.tsx
+++ src/components/ButtonSection.tsx
@@ -1,19 +1,24 @@
 import {Link} from "react-router-dom";
+import React from "react";
 
 
 interface Props {
-    
+    forecastSection: "today" | "tomorrow";
+    setForecastSection: React.Dispatch<React.SetStateAction<"today" | "tomorrow">>
 }
 
 
-export default function ButtonSection(){
+export default function ButtonSection({forecastSection, setForecastSection}: Props){
     return (
         <div className="px-10 flex items-baseline justify-between">
             <button className="text-white font-semibold text-sm flex flex-col items-center gap-y-1">
                 Today
-                <div className="bg-white rounded-full h-1.5 w-1.5"></div>
+                {forecastSection === "today" && <div className="bg-white rounded-full h-1.5 w-1.5"></div>}
             </button>
-            <button className="text-white font-semibold text-sm">Tomorrow</button>
+            <button className="text-white font-semibold text-sm f   lex flex-col items-center gap-y-1">
+                Tomorrow
+                {forecastSection === "tomorrow" && <div className="bg-white rounded-full h-1.5 w-1.5"></div>}
+            </button>
             <Link to="/next-seven-days"
                 className="text-swatch_1 font-semibold text-sm flex items-center justify-center">
                 Next 7 Days
