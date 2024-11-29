3a775b7781c44347cc38b1126a5a4ac9f50c9994
iamwinner422 <iamwinner422@gmail.com>
2024-11-29 14:48:18 +0000

    adding isNight prop to NextSeventDays

--- src/pages/NextSevenDays.tsx
+++ src/pages/NextSevenDays.tsx
@@ -2,11 +2,15 @@ import Title from "../components/Title.tsx";
 import FrameLayout from "../layouts/FrameLayout.tsx";
 import BaseLayout from "../layouts/BaseLayout.tsx";
 
-export default function NextSevenDays(){
+
+interface Props {
+    isNight: boolean;
+}
+export default function NextSevenDays({isNight}: Props){
     return(
         <BaseLayout>
-            <FrameLayout>
-                <Title/>
+            <FrameLayout isNight={isNight}>
+                <Title isNight={isNight}/>
             </FrameLayout>
         </BaseLayout>
     )
