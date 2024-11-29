db873dc22af186434e5a80d2b57afa793f7b1071
iamwinner422 <iamwinner422@gmail.com>
2024-11-29 11:03:12 +0000

    displaying data

--- src/components/ForecastDetail.tsx
+++ src/components/ForecastDetail.tsx
@@ -1,4 +1,6 @@
 import {WeatherData} from "../lib/types.ts";
+import moment from "moment";
+
 
 interface Props {
     data: WeatherData
@@ -6,12 +8,12 @@ interface Props {
 export default function ForecastDetail({data}: Props) {
     return (
         <>
-            <span style={{fontSize: "10px"}} className="text-white group-hover:text-swatch_2 font-bold uppercase text-center">12AM</span>
+            <span style={{fontSize: "10px"}} className="text-white group-hover:text-swatch_2 font-bold uppercase text-center">{moment(data.time).format('hA')}</span>
             <div className="bg-appBackground/40 group-hover:bg-swatch_3 mx-auto h-7 w-7 rounded-full flex items-center justify-center my-1">
                 <i className="wi wi-sunset text-white group-hover:text-swatch_1 text-center"></i>
             </div>
             <div style={{fontSize: "12px"}} className="flex items-center justify-center group-hover:text-swatch_2 text-white font-bold">
-                29 <span style={{fontSize: "9px"}} className="text-xs ml-1 text-center ">°C</span>
+                {data.values.temperature} <span style={{fontSize: "9px"}} className="text-xs ml-1 text-center ">°C</span>
             </div>
         </>
     )
