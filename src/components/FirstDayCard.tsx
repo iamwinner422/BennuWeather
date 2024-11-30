86ad7e10e38cc9cb28f912940fe1930a462309b2
iamwinner422 <iamwinner422@gmail.com>
2024-11-30 11:30:21 +0000

    ...

--- src/components/FirstDayCard.tsx
+++ src/components/FirstDayCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 }
 export default function FirstDayCard({firstData, isFetching}: Props){
     return (
-        <div className="bg-white shadow-xl rounded px-4 py-6 flex flex-col gap-y-5">
+        <div className="bg-white shadow-xl rounded-lg px-4 py-6 flex flex-col gap-y-5">
             <div className="flex justify-between items-center">
                 <div className="flex gap-x-4 items-center">
                     <h5 className="text-sm text-appBackground font-bold">{isFetching ? "--" : moment(firstData?.time).format('dddd')}</h5>
